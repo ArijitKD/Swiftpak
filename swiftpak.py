e44ec9961a385a593df7f92a957c44544e3bb4f4
Arijit Kumar Das <arijitkrdas2004@outlook.com>
2023-10-17 23:30:49 +0530

    Update swiftpak.py

--- swiftpak.py
+++ swiftpak.py
@@ -1,21 +1,23 @@
-import tkinter as tk, tkinter.ttk as ttk, platform
+import tkinter as tk, tkinter.ttk as ttk, platform, tkinter.font as font
 
 ## CONSTANTS
 APP_NAME = "Swiftpak" # The default title for Regalter that
                       # will appear on the title bar.
 WINDOW_BG_COLOR = '#f6f5f4'
+SYSTEM = platform.system()
 
 class GUI:
     def __init__(self):
 
-        #Detect the current system
-        self.system = platform.system()
-
         # Creating the root window
         self.root_window = tk.Tk()
 
-        style = ttk.Style(self.root_window)
-        style.theme_use('clam')
+        self.font = font.Font(family='Arial', size=10)
+
+        if (SYSTEM == "Windows"):
+            self.font.configure(size=11)
+        if (SYSTEM != "Windows"):
+            ttk.Style(self.root_window).theme_use('clam')
 
         # Screen dimensions
         self.screen_width = self.root_window.winfo_screenwidth()
@@ -44,16 +46,26 @@ class GUI:
         self.root_window.protocol("WM_DELETE_WINDOW", self.close_root_window)
 
         # Creating the "Choose file:" label with appropriate attributes
-        self.owner_label = ttk.Label(self.root_window, text = 'Choose file:', font=('normal', 10, 'normal'), background=WINDOW_BG_COLOR)
-        self.owner_label.place(x=20, y=20)
-
-        self.owner_entry = ttk.Entry(self.root_window, textvariable = None, font=('segoe ui', 11,'normal'), state='normal', cursor='arrow')
-        self.owner_entry.place(x=150, y=15, width = 300)
+        self.choose_file_label = ttk.Label(self.root_window, text = 'Choose file:', font=self.font, background=WINDOW_BG_COLOR)
+        self.choose_file_label.place(x=20, y=20)
 
 
+        self.file_entry = ttk.Entry(self.root_window, textvariable = None, font=self.font, state='normal', width=34)
+        if (SYSTEM == "Windows"):
+            self.file_entry.configure(width=40)
+        self.file_entry.place(x=20, y=45)
+        self.file_entry.focus_set()
+
+        style = ttk.Style()
+        style.configure('my.TButton', font=self.font)
+        self.browse_button = ttk.Button(self.root_window, text="Browse...", style="my.TButton")
+        if (SYSTEM == "Windows"):
+            self.browse_button.place(x=362, y=42)
+        else:
+            self.browse_button.place(x=343, y=36)
 
     def load_settings(self):
-        if (self.system == "Windows"):
+        if (SYSTEM == "Windows"):
             pass
 
     def close_root_window(self):
